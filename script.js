086d00fd4d5ac48c1941e743e066cd6a5f7f3bc2
dhekra2023 <dhekra202@gmail.com>
2024-07-09 13:08:30 +0100

    done

---
+++ script.js
@@ -0,0 +1,52 @@
+// open and close modal
+var modal = document.getElementById("modal");
+
+var openModal = document.getElementById("openModalBtn");
+
+var closeModal = document.getElementById("closeModalBtn");
+
+
+openModal.onclick = function() {
+    modal.style.display = "block";
+}
+
+closeModal.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+// 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+// slide show images
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+$(".login-button").onclick(function(){
+  alert("login succeded!")
+})
